82bed750b1b832a049a924088c672d95ee6cef8f
flowlife <34335820+flowlife@users.noreply.github.com>
2021-04-20 23:30:36 +0800

    Update and rename diy-part1.sh to diy-part1-lienol.sh

--- diy-part1-lienol.sh
+++ diy-part1-lienol.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
