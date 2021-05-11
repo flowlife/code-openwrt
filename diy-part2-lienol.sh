cf95f8083756e6de93fae5ba0d56f5e455718094
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-12 02:53:13 +0800

    Update diy-part2-lienol.sh

--- diy-part2-lienol.sh
+++ diy-part2-lienol.sh
@@ -14,4 +14,3 @@
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /flowlife build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
