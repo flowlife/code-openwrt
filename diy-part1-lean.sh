2e058cbac9f1fd8d873d16ece19755b2944abbe1
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-11 21:21:07 +0800

    Update diy-part1-lean.sh

--- diy-part1-lean.sh
+++ diy-part1-lean.sh
@@ -13,8 +13,8 @@
 # Uncomment a feed source
 git clone https://github.com/flowlife/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
-rm -rf /openwrt/package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/openwrt-packages  
+rm -rf /openwrt/package/openwrt/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
