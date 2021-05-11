cef823a2182748407b3557406f5e9b3be173f341
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-11 21:42:12 +0800

    Add files via upload

---
+++ Scripts/AutoBuild_DiyScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# https://github.com/Hyy2001X/AutoBuild-Actions
+# AutoBuild Module by Hyy2001
+# AutoBuild DiyScript
+
+Diy_Core() {
+	Author=Hyy2001
+	Default_Device=x86_64
+	Short_Firmware_Date=true
+	Default_IP_Address=192.168.1.1
+
+}
+
