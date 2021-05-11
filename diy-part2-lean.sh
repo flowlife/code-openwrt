bebf15b89cd1b71cbd702c112efd4a92b2072386
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-12 00:41:32 +0800

    Update diy-part2-lean.sh

--- diy-part2-lean.sh
+++ diy-part2-lean.sh
@@ -1,2 +1,3 @@
+sed -i "s/OpenWrt /Flowlife/g" package/lean/default-settings/files/zzz-default-settings
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
