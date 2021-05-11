14c475c7e9f654830ade38ef22405ed17eeeb405
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-12 00:46:05 +0800

    Update diy-part2-lean.sh

--- diy-part2-lean.sh
+++ diy-part2-lean.sh
@@ -1,3 +1,4 @@
-sed -i "s/OpenWrt /Flowlife/g" package/lean/default-settings/files/zzz-default-settings
+# 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+sed -i "s/OpenWrt /flowlife build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
