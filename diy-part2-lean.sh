5634f261d149ab272220f031e4623f46e6580352
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-12 18:37:14 +0800

    Update diy-part2-lean.sh

--- diy-part2-lean.sh
+++ diy-part2-lean.sh
@@ -1,2 +1,2 @@
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i "s/OpenWrt /Compiled by Flowlife $(TZ=UTC-8 date "+%Y.%m.%d") / OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /Compiled by Flowlife $(TZ=UTC-8 date "+%Y.%m.%d") ® OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
