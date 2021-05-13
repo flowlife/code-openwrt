817df9663918eed47555f0722650bbce4455907d
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-13 11:43:38 +0800

    Update diy-part1-lean.sh

--- diy-part1-lean.sh
+++ diy-part1-lean.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-git clone https://github.com/flowlife/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/flowlife/luci-app-onliner.git package/openwrt-packages
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
