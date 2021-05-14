845302ac0e6f0e8f446355a236fc79e0e5147d5f
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-14 12:18:25 +0800

    Create diy-part2-lean-vssr.sh

---
+++ diy-part2-lean-vssr.sh
@@ -0,0 +1,3 @@
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i "s/OpenWrt /Compiled by Flowlife $(TZ=UTC-8 date "+%Y.%m.%d") ™ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
