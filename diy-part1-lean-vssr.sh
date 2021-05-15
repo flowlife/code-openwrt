00c76c5b1c09213d6afd7d6c863b87dd1274ee04
flowlife <34335820+flowlife@users.noreply.github.com>
2021-05-16 07:39:16 +0800

    Update diy-part1-lean-vssr.sh

--- diy-part1-lean-vssr.sh
+++ diy-part1-lean-vssr.sh
@@ -1,4 +1,3 @@
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/flowlife/luci-app-onliner.git package/luci-app-onliner
-git clone https://github.com/tsl0922/ttyd.git package/luci-app-ttyd
