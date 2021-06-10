ec3e89669ed35548ccab7128b92603a64cda8da5
flowlife <34335820+flowlife@users.noreply.github.com>
2021-06-10 19:38:46 +0800

    Update diy-part1-lean-vssr.sh

--- diy-part1-lean-vssr.sh
+++ diy-part1-lean-vssr.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/flowlife/luci-app-onliner.git package/luci-app-onliner
-git clone -b 18.06 https://github.com/flowlife/luci-app-filebrowser.git package/luci-app-filebrowser
+git clone https://github.com/flowlife/luci-app-filebrowser.git package/luci-app-filebrowser
 sed -i 's/src-git packages https:\/\/github.com\/coolsnowwolf\/packages/src-git packages https:\/\/github.com\/flowlife\/packages/g' feeds.conf.default
