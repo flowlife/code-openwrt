b4bee5ba3fb5e388c61e84ad84f48fc97a02ef4b
flowlife <34335820+flowlife@users.noreply.github.com>
2021-06-12 00:30:59 +0800

    Update diy-part1-lean1907.sh

--- diy-part1-lean1907.sh
+++ diy-part1-lean1907.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/flowlife/luci-app-onliner.git package/luci-app-onliner
-sed -i 's/src-git packages https:\/\/github.com\/coolsnowwolf\/packages/src-git packages https:\/\/github.com\/flowlife\/packages;openwrt-19.07/g' feeds.conf.default
+#sed -i 's/src-git packages https:\/\/github.com\/coolsnowwolf\/packages/src-git packages https:\/\/github.com\/flowlife\/packages;openwrt-19.07/g' feeds.conf.default
