425190fc7614eb93278105227997cea9a6e0890d
flowlife <34335820+flowlife@users.noreply.github.com>
2021-06-12 09:00:35 +0800

    Delete diy-part2-lean1907.sh

--- diy-part2-lean1907.sh
+++
@@ -1,7 +0,0 @@
-# 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-
-#rm -rf package/lean/luci-theme-argon
-
-#git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-sed -i "s/OpenWrt /Compiled by Flowlife $(TZ=UTC-8 date "+%Y.%m.%d") ™ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
