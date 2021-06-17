e68ddfacb16b22299f38e82933dbcd04c443a885
flowlife <34335820+flowlife@users.noreply.github.com>
2021-06-17 11:50:47 +0800

    Update diy-part2-lean.sh

--- diy-part2-lean.sh
+++ diy-part2-lean.sh
@@ -1,7 +1,7 @@
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 
-#删除argon主题
-#rm -rf package/lean/luci-theme-argon
+#删除自带argon主题
+rm -rf package/lean/luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i "s/OpenWrt /OpenWrt Compiled by Flowlife $(TZ=UTC-8 date "+%Y.%m.%d") ™/g" package/lean/default-settings/files/zzz-default-settings
