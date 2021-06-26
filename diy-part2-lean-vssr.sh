f79244d61f6e14b562e76530c3121b0322ec62ae
flowlife <34335820+flowlife@users.noreply.github.com>
2021-06-26 16:52:14 +0800

    Update diy-part2-lean-vssr.sh

--- diy-part2-lean-vssr.sh
+++ diy-part2-lean-vssr.sh
@@ -3,7 +3,7 @@ rm -rf package/lean/luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i "s/OpenWrt /OpenWrt Compiled By Flowlife $(TZ=UTC-8 date "+%Y.%m.%d") ™/g" package/lean/default-settings/files/zzz-default-settings
-cp -f package/DIY-openwrt/DIY-lean/banner package/base-files/files/etc/banner
+#cp -f package/DIY-openwrt/DIY-lean/banner package/base-files/files/etc/banner
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
 #修改默认ip
